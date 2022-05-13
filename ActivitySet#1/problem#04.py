94e046901cc07645137c93996ca43ea693bad36f
Shreyans45 <94587906+Shreyans45@users.noreply.github.com>
2022-05-13 20:36:13 +0530

    OP

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,9 @@
-# Conditional Execution
-
-hrs = input("Enter hours? ")
+hrs = float(input("Enter Hours:"))
+a=float(40)
+diff=hrs-a
+if (hrs > a):
+    grass=40*10.5+(diff*10.5)*1.5
+    print(grass)
+else :
+    grass=(hrs)*1.5
+    print(grass)

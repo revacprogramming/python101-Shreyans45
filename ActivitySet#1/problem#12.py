2a25ad3bdd5f5356b3768fed9d449cfea679bfaa
Shreyans45 <94587906+Shreyans45@users.noreply.github.com>
2022-05-30 20:07:01 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,13 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+fname = input("Enter file name")
+fh = open(fname)
+counts = dict()
+lst = []
+for line in fh:
+    if not line.startswith("From "):
+        continue
+    line = line.rstrip().split()
+    emails = line[1]
+    lst.append(emails)
+for email in lst:
+    counts[email] = counts.get(email, 0) +1
+print(emails, counts[email])

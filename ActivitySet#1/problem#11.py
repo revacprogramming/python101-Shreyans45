230bd960ac0fb1f3bd354c31337b863a26348e9a
Shreyans45 <94587906+Shreyans45@users.noreply.github.com>
2022-05-30 20:27:32 +0530

    Update problem#11.py

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,23 @@
-# Tuples
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+x=handle.read()
+l=[]
+d={}
+for line in handle:
+    if not line.startswith("From "):
+        
+        continue
+    if line.startswith("From"):
+        y=line.split()
+        a=y[5]
+	maggy=a.split(':')
+    maharshi=maggy[0]
+    l.append(maharshi)
+    l.sort()
+for word in l:
+    d[word]=d.get(word,0)+1
 
-filename = "dataset/mbox-short.txt"
+for a,b in d.items():
+    print(a,b)
